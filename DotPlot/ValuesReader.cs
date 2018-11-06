b56e525f65639dac7bce28653323a84d4f45ee91
Teerachai Laothong <tlaothong@gmai.com>
2018-11-06 21:21:51 +0700

    Added ValuesReader

---
+++ DotPlot/ValuesReader.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace DotPlot
+{
+    public class ValuesReader {
+        public IEnumerable<int> ReadValuesFromDataFile(string fullPathToFile) {
+            using (var fi = File.OpenText(fullPathToFile))
+            {
+                var values = new List<int>();
+                string line;
+                while ((line = fi.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        values.Add(int.Parse(line));
+                    }
+                }
+
+                return values;
+            }
+        }
+    }
+}
