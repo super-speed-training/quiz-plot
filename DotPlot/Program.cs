869b299fa9ae0cc310f463ca7d741d17c5da5f90
Teerachai Laothong <tlaothong@gmai.com>
2018-11-06 21:26:43 +0700

    Added TODOs to Main

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace DotPlot
 {
     class Program
     {
+        // TODO: Replace this string value with the path to actual data file.
+        private const string FullPathToDataFile = @"Values.Data.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("DOT Plot");
+            Console.WriteLine();
+
+            // TODO: Implement quiz here
         }
     }
 }
