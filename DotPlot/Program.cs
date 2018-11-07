d5dd4928674f958b3517aa46b01f77c85a3d7b61
Anawat <anawat@perfenterprise.com>
2018-11-07 09:53:47 +0700

    implement code
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -13,7 +13,34 @@ namespace DotPlot
             Console.WriteLine("DOT Plot");
             Console.WriteLine();
 
-            // TODO: Implement quiz here
+            var path = @"..\..\..\..\data\Values.Data.txt";
+            var ValueReader = new ValuesReader();
+            var data = ValueReader.ReadValuesFromDataFile(path);
+            var mean = FindMean(data);
+
+            for (int i = 10; i >= 0; i--)
+            {
+                if (i != 10)
+                    Console.Write(" ");
+                Console.Write($"{i} |");
+                foreach (var item in data)
+                {
+                    if (item == i)
+                        Console.Write("*");
+                    else if (i == mean)
+                        Console.Write("-");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("=====================================================");
+            Console.ReadKey();
+        }
+
+        static int FindMean(int[] values)
+        {
+            return values.Sum() / values.Count();
         }
     }
 }
