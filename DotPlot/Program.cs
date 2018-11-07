2df4116f90f6c34999a2c0c128efb5d7eaf711c9
Perawatt <pekung4433@perfenterprise.com>
2018-11-07 11:04:40 +0700

    complete perawatt

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -6,19 +6,38 @@ namespace DotPlot
     class Program
     {
         // TODO: Replace this string value with the path to actual data file.
-        private const string FullPathToDataFile = @"../../../../data/Values.Data.txt";
+        private const string FullPathToDataFile = @"F:\Git\quiz-plot\data\Values.Data.txt";
 
         static void Main(string[] args)
         {
             var svc = new ValuesReader();
             var data = svc.ReadValuesFromDataFile(FullPathToDataFile);
             var average = FindMean(data);
-            
-            for (int i = 0; i < data.Length; i++)
+
+            for (int vectorY = 10; vectorY >= 0; vectorY--)
             {
-                Console.WriteLine(data[i]);
-            };
-            // TODO: Implement quiz here
+                Console.Write($"{vectorY,4:G}"+"|");
+                for (int vectorX = 0; vectorX < data.Length; vectorX++)
+                {
+                    if (data[vectorX] == vectorY)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if(vectorY == average)
+                        {
+                            Console.Write("-");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.Write(new String('=', data.Length+5));
             Console.ReadLine();
         }
         static int FindMean(int[] values)
@@ -30,6 +49,5 @@ namespace DotPlot
             }
             return valueAverage / values.Length;            
         }
-
     }
 }
