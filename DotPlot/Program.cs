1acc429e55f507ef8fe640c88fbfce3c25aac13e
Somkiet <somsor@perfenterprise.com>
2018-11-07 09:59:29 +0700

    decorate

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -24,11 +24,16 @@ namespace DotPlot
                 graph[max - values[i], i] = 1;
             }
 
-            for (int i = 0; i < graphHight; i++)
+            var mean = values.Sum() / values.Count();
+
+            for (int i = 0; i < graphHight + 1; i++)
             {
+                Console.Write($"{(graphHight - i).ToString(" 0 |")}");
                 for (int j = 0; j < graphWidth; j++)
                 {
-                    if (graph[i, j] == 0)
+                    if (graph[i, j] == 0 && i - 1 == mean)
+                        Console.Write("-");
+                    else if (graph[i, j] == 0)
                         Console.Write(" ");
                     else
                         Console.Write("*");
