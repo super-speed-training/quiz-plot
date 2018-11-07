707cd791698486f598b7bc1fed2e97a590ae327d
Somkiet <somsor@perfenterprise.com>
2018-11-07 09:55:11 +0700

    plot graph

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -10,10 +10,31 @@ namespace DotPlot
 
         static void Main(string[] args)
         {
-            Console.WriteLine("DOT Plot");
-            Console.WriteLine();
+            var reader = new ValuesReader();
+            var values = reader.ReadValuesFromDataFile(FullPathToDataFile);
 
-            // TODO: Implement quiz here
+            var max = values.Max();
+            var min = values.Min();
+            var graphHight = max;
+            var graphWidth = values.Count();
+
+            var graph = new int[graphHight + 1, graphWidth];
+            for (int i = 0; i < graphWidth; i++)
+            {
+                graph[max - values[i], i] = 1;
+            }
+
+            for (int i = 0; i < graphHight; i++)
+            {
+                for (int j = 0; j < graphWidth; j++)
+                {
+                    if (graph[i, j] == 0)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
