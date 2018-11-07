c409b429989210c1cd6117fcb8adb547e7438dee
SorrY <so.sikarin@perfenterprise.com>
2018-11-07 10:02:51 +0700

    don't work

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Linq;
 
-namespace DotPlot
-{
-    class Program
-    {
+namespace DotPlot {
+    class Program {
         // TODO: Replace this string value with the path to actual data file.
         private const string FullPathToDataFile = @"Values.Data.txt";
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("DOT Plot");
-            Console.WriteLine();
+        static void Main (string[] args) {
+            var ans = new ValuesReader ();
+            int[] input = ans.ReadValuesFromDataFile (FullPathToDataFile);
+            Console.WriteLine ("DOT Plot");
+            foreach (var item in input)
+            {
+               Console.WriteLine (item); 
+            }
+            
 
             // TODO: Implement quiz here
         }
