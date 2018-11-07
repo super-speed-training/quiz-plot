979452b144acb504e9282a96893714a8e0c6c937
unravel07 <mick_ben10@hotmail.com>
2018-11-07 10:02:35 +0700

    mick

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -8,12 +8,27 @@ namespace DotPlot
         // TODO: Replace this string value with the path to actual data file.
         private const string FullPathToDataFile = @"Values.Data.txt";
 
+
         static void Main(string[] args)
         {
             Console.WriteLine("DOT Plot");
-            Console.WriteLine();
+            // var values = new ValuesReader();
+            // var keepValue = values.ReadValuesFromDataFile(FullPathToDataFile);
 
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i + "  |");
+            }
+            Console.WriteLine("==============================================");
             // TODO: Implement quiz here
         }
     }
 }
+// for (int i = 0; i<11; i++)
+//     {
+//         for()
+//         {
+//             Console.WriteLine(i + "  |");
+//         }
+        
+//     }
