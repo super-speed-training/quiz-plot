47a7a97669c24b2f9ecd59a3b741af592547515b
thaninpong <thaninpong@perfenterprise.com>
2018-11-07 10:02:47 +0700

    Implement
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -3,17 +3,24 @@ using System.Linq;
 
 namespace DotPlot
 {
-    class Program
+    public class Program
     {
         // TODO: Replace this string value with the path to actual data file.
         private const string FullPathToDataFile = @"Values.Data.txt";
 
+
         static void Main(string[] args)
         {
+            // TODO: Implement quiz here
             Console.WriteLine("DOT Plot");
             Console.WriteLine();
+            var ValuesReader = new ValuesReader();
+            var datas = ValuesReader.ReadValuesFromDataFile(FullPathToDataFile);
+        }
+        static int FindMean(int[] values){
+            var avg = values.Average();
 
-            // TODO: Implement quiz here
+            return 0;
         }
     }
 }
