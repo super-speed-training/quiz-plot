68dc763d7fc1aadd94a7b41874032fae12468178
woraput <woraput@kkumail.com>
2018-11-07 10:02:22 +0700

    final

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
+
 
 namespace DotPlot
 {
     class Program
     {
         // TODO: Replace this string value with the path to actual data file.
-        private const string FullPathToDataFile = @"Values.Data.txt";
+        private const string FullPathToDataFile =(@"d:\work diamond\Hackatron-Holiday\Quiz4\quiz-plot\data\Values.Data.txt");
 
         static void Main(string[] args)
         {
-            Console.WriteLine("DOT Plot");
-            Console.WriteLine();
 
+            Console.WriteLine("DOT Plot");
+            Console.WriteLine(File.ReadLines(FullPathToDataFile));
+            
+            
+            
             // TODO: Implement quiz here
         }
     }
