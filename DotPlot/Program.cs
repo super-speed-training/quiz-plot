e34670789256f9d45777ea35ea9601c8a3dd501d
Perawatt <pekung4433@perfenterprise.com>
2018-11-07 10:02:38 +0700

    incomplete

--- DotPlot/Program.cs
+++ DotPlot/Program.cs
@@ -6,14 +6,30 @@ namespace DotPlot
     class Program
     {
         // TODO: Replace this string value with the path to actual data file.
-        private const string FullPathToDataFile = @"Values.Data.txt";
+        private const string FullPathToDataFile = @"../../../../data/Values.Data.txt";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("DOT Plot");
-            Console.WriteLine();
-
+            var svc = new ValuesReader();
+            var data = svc.ReadValuesFromDataFile(FullPathToDataFile);
+            var average = FindMean(data);
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
             // TODO: Implement quiz here
+            Console.ReadLine();
         }
+        static int FindMean(int[] values)
+        {
+            int valueAverage = 0;
+            for (int i = 0; i < values.Length ; i++)
+            {
+                valueAverage += values[i];
+            }
+            return valueAverage / values.Length;            
+        }
+
     }
 }
